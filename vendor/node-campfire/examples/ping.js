4ec5686260a9602c6a199d132a39f0ed19481a8d
Jon Ursenbach <lorderunion@gmail.com>
2011-06-19 22:22:07 -0700

    Braid: Update mirror 'vendor/node-campfire' to '91845b1'

--- vendor/node-campfire/examples/ping.js
+++ vendor/node-campfire/examples/ping.js
@@ -1,25 +1,22 @@
-var system   = require('sys');
-var Campfire = require('../lib/campfire').Campfire;
+var Campfire = require("../lib/campfire").Campfire;
 
 var instance = new Campfire({
   ssl     : false,
-  token   : 'YOUR_TOKEN',
-  account : 'YOUR_ACCOUNT'
+  token   : "YOUR_TOKEN",
+  account : "YOUR_ACCOUNT"
 });
 
-instance.room(ROOM_ID_OR_NAME, function(room) {
-  room.join(function() {
-    room.listen(function(message) {
-      if (message.body == 'PING') {
-        system.puts('PING received.');
+instance.join(ROOM_ID, function(error, room) {
+  room.listen(function(message) {
+    if (message.body == "PING") {
+      console.log("PING received.");
 
-        room.speak('PONG', function(data) {
-          system.puts('PONG sent at ' + data.message.created_at + '.');
-        });
-      } else {
-        system.puts('Received unknown message:');
-        system.puts(system.inspect(message));
-      }
-    });
+      room.speak("PONG", function(error, response) {
+        console.log("PONG sent at " + response.message.created_at + ".");
+      });
+    } else {
+      console.log("Received unknown message:");
+      console.log(message);
+    }
   });
 });
--- vendor/node-campfire/lib/campfire.js
+++ vendor/node-campfire/lib/campfire.js
@@ -1,78 +1,110 @@
-var http   = require('http');
-var https  = require('https');
-var encode = require('../vendor/base64').encode;
+var Room    = require("./campfire/room").Room,
+    Message = require("./campfire/message").Message;
 
 var Campfire = function(options) {
-  this.ssl           = options.ssl;
-  this.http          = this.ssl ? https : http;
-  this.port          = this.ssl ? 443 : 80;
-  this.token         = options.token;
-  this.account       = options.account;
-  this.domain        = this.account + '.campfirenow.com';
-  this.authorization = 'Basic ' + encode(this.token + ':x');
+  var options = options || {},
+      ssl     = !!options.ssl;
+
+  if (!options.token)   { throw new Error("Please provide an API token."); }
+  if (!options.account) { throw new Error("Please provide an account name."); }
+
+  this.http          = ssl ? require("https") : require("http");
+  this.port          = ssl ? 443 : 80;
+  this.domain        = options.account + ".campfirenow.com";
+  this.authorization = "Basic " + new Buffer(options.token + ":x").toString("base64");
 };
 
-Campfire.prototype.rooms = function(callback) {
-  var self = this;
-  this.get('/rooms', function(data) {
-    if (typeof data != 'object') {
-      throw new Error('The call to Campfire.rooms failed. Verify that your credentials are correct.');
+Campfire.prototype.join = function(id, callback) {
+  this.room(id, function(error, room) {
+    if (error) {
+      return callback(error);
     }
 
-    callback(data.rooms.map(function(roomData) {
-      return new Campfire.Room(self, roomData);
-    }));
+    room.join(function(error) {
+      callback(error, room);
+    });
   });
-};
-
-Campfire.prototype.room = function(idOrName, callback) {
-  if (typeof(callback) != 'function') {
-    throw new Error('A callback must be provided for retrieving a room.');
-  }
-
-  this.rooms(function(rooms) {
-    for (var i = rooms.length - 1; i >= 0; i--) {
-      var room = rooms[i];
-
-      if (room.id == idOrName || room.name == idOrName) {
-        callback(room);
-        return;
-      }
-    }
-
-    callback();
-  });
-};
-
-Campfire.prototype.user = function(id, callback) {
-  this.get('/users/' + id, callback);
 };
 
 Campfire.prototype.me = function(callback) {
-  this.get('/users/me', callback);
+  this.get("/users/me", callback);
+};
+
+Campfire.prototype.presence = function(callback) {
+  this.get("/presence", function(error, response) {
+    if (response) {
+      var rooms = response.rooms.map(function(room) {
+        return new Room(this, room);
+      }, this);
+    }
+
+    callback(error, rooms);
+  }.bind(this));
+};
+
+Campfire.prototype.rooms = function(callback) {
+  this.get("/rooms", function(error, response) {
+    if (response) {
+      var rooms = response.rooms.map(function(room) {
+        return new Room(this, room);
+      }, this);
+    }
+
+    callback(error, rooms);
+  }.bind(this));
+};
+
+Campfire.prototype.room = function(id, callback) {
+  this.get("/room/" + id, function(error, response) {
+    if (response) {
+      var room = new Room(this, response.room);
+    }
+
+    callback(error, room);
+  }.bind(this));
+};
+
+Campfire.prototype.search = function(term, callback) {
+  this.get("/search/" + term, function(error, response) {
+    if (response) {
+      var messages = response.messages.map(function(message) {
+        return new Message(this, message);
+      }, this);
+    }
+
+    callback(error, messages);
+  }.bind(this));
+};
+
+Campfire.prototype.user = function(id, callback) {
+  this.get("/users/" + id, callback);
+};
+
+Campfire.prototype.delete = function(path, callback) {
+  this.request("DELETE", path, "", callback);
 };
 
 Campfire.prototype.get = function(path, callback) {
-  this.request('GET', path, null, callback);
+  this.request("GET", path, null, callback);
 };
 
 Campfire.prototype.post = function(path, body, callback) {
-  this.request('POST', path, body, callback);
+  this.request("POST", path, body, callback);
 };
 
 Campfire.prototype.request = function(method, path, body, callback) {
   var headers = {
-    'Authorization' : this.authorization,
-    'Host'          : this.domain,
-    'Content-Type'  : 'application/json'
+    "Authorization" : this.authorization,
+    "Host"          : this.domain,
+    "Content-Type"  : "application/json"
   };
 
-  if (method == 'POST') {
-    if (typeof(body) != 'string') {
+  if (method == "POST" || method == "DELETE") {
+    if (typeof(body) != "string") {
       body = JSON.stringify(body);
     }
 
-    headers['Content-Length'] = body.length;
+    headers["Content-Length"] = body.length;
   }
 
   var options = {
@@ -84,122 +116,29 @@ Campfire.prototype.request = function(method, path, body, callback) {
   };
 
   var request = this.http.request(options, function(response) {
-    if (typeof(callback) == 'function') {
-      var data = '';
+    if (callback instanceof Function) {
+      var data = "";
 
-      response.on('data', function(chunk) {
+      response.on("data", function(chunk) {
         data += chunk;
       });
-      response.on('end', function() {
+      response.on("end", function() {
         try {
           data = JSON.parse(data);
-        } catch(e) {}
+        } catch(e) {
+          return callback(new Error("Invalid JSON response."));
+        }
 
-        callback(data);
+        callback(null, data);
       });
     }
   });
 
-  if (method == 'POST') {
+  if (method == "POST") {
     request.write(body);
   }
 
   request.end();
-};
-
-Campfire.Room = function(campfire, data) {
-  this.id              = data.id;
-  this.name            = data.name;
-  this.topic           = data.topic;
-  this.locked          = data.locked;
-  this.createdAt       = data.created_at;
-  this.updatedAt       = data.updated_at;
-  this.membershipLimit = data.membership_limit;
-
-  this.path     = '/room/' + this.id;
-  this.campfire = campfire;
-};
-
-Campfire.Room.prototype.join = function(callback) {
-  this.post('/join', '', callback);
-};
-
-Campfire.Room.prototype.leave = function(callback) {
-  this.post('/leave', '', callback);
-};
-
-Campfire.Room.prototype.listen = function(callback) {
-  if (typeof(callback) != 'function') {
-    throw new Error('A callback must be provided for listening.');
-  }
-
-  var
-  campfire = this.campfire,
-  options  = {
-    host    : 'streaming.campfirenow.com',
-    port    : campfire.port,
-    method  : 'GET',
-    path    : this.path + '/live.json',
-    headers : {
-      'Host'          : 'streaming.campfirenow.com',
-      'Authorization' : campfire.authorization
-    }
-  };
-
-  campfire.http.request(options, function(response) {
-    response.setEncoding('utf8');
-    response.on('data', function(chunk) {
-      if (chunk.trim() == '') {
-        return;
-      }
-
-      chunk = chunk.split("\r");
-
-      for (var i = 0; i < chunk.length; ++i) {
-        if (chunk[i].trim() != '') {
-          try {
-            callback(JSON.parse(chunk[i]));
-          } catch(e) {}
-        }
-      }
-    });
-  }).end();
-},
-
-Campfire.Room.prototype.speak = function(text, callback) {
-  this.message(text, 'TextMessage', callback);
-};
-
-Campfire.Room.prototype.paste = function(text, callback) {
-  this.message(text, 'PasteMessage', callback);
-};
-
-Campfire.Room.prototype.sound = function(text, callback) {
-  this.message(text, 'SoundMessage', callback);
-},
-
-Campfire.Room.prototype.message = function(text, type, callback) {
-  this.post('/speak', { message : { body : text, type : type } }, callback);
-};
-
-Campfire.Room.prototype.lock = function(callback) {
-  this.post('/lock', '', callback);
-};
-
-Campfire.Room.prototype.unlock = function(callback) {
-  this.post('/unlock', '', callback);
-};
-
-Campfire.Room.prototype.show = function(callback) {
-  this.post('', '', callback);
-};
-
-Campfire.Room.prototype.get = function(path, callback) {
-  this.campfire.get(this.path + path, callback);
-};
-
-Campfire.Room.prototype.post = function(path, body, callback) {
-  this.campfire.post(this.path + path, body, callback);
 };
 
 exports.Campfire = Campfire;
